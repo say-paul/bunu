79053a827b60e144569c8b9bbd40ad89fe13041f
saypaul <saypaul@redhat.com>
2020-04-24 17:10:26 +0530

    first commit

---
+++ what_to_learn.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 24 16:19:29 2020
+
+@author: jhilikkundu
+"""
+
+a=int(input("enter a number-"))
+b=int(input("enter 2nd no-"))
+c=a+b
+print(c)
