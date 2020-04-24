78a0d8b2b9f0216d6296ca1bb49ecdde3e757aa6
saypaul <saypaul@redhat.com>
2020-04-24 17:25:16 +0530

    updated code

--- what_to_learn.py
+++ what_to_learn.py
@@ -6,7 +6,21 @@ Created on Fri Apr 24 16:19:29 2020
 @author: jhilikkundu
 """
 
-a=int(input("enter a number-"))
-b=int(input("enter 2nd no-"))
-c=a+b
-print(c)
+import sys
+def add(a,b):
+# a=int(input("enter a number-"))
+# b=int(input("enter 2nd no-"))
+    return a+b
+
+
+
+def main(argv):
+    if len(argv) != 3:
+        print('Enter two numeric value')
+        raise Exception('Incorrect number of arguments')
+    a = int(argv[1])
+    b = int(argv[2])
+    print (' output is {}'.format(add(a,b)))
+
+if __name__ == '__main__':
+    main(sys.argv)
