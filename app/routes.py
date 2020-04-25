25d0b73b66a7af86432d6005cd492a46fe04255c
saypaul <saypaul@redhat.com>
2020-04-25 20:35:25 +0530

    flask adding functions

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import Flask
+from basic_func.func1 import addition
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
     return '<!DOCTYPE html > \
     <html > \
@@ -12,6 +12,14 @@ def hello_world():
     </body> \
     </html>'   
 
+
+@app.route('/sum')
+def sumofnumbers():
+    b = "sum of 5 and 7 is:" + str(addition(5, 7))
+    print (b)
+    return b
+
+    
 
 if __name__ == '__main__':
    app.run()
