3434e2b81d2ce48838f0260425907e5cfc27a363
jhilikkundu <jhilikkundu@localhost.localdomain>
2020-04-26 12:15:59 +0530

    added prime

---
+++ app/basic_func/prime.py
@@ -0,0 +1,9 @@
+def prime(n):
+    n=int(n)
+    for i in range(2,n//2):
+        if n%i==0:
+            return("non prime")
+            break
+    else:
+        return("prime")
+print(prime(-23))
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
+from basic_func.prime import prime
 from basic_func.func1 import addition
 app = Flask(__name__)
 
@@ -17,10 +18,18 @@ def hello_world():
 def sumofnumbers():
     val1 = request.args.get('a',type = str)
     val2 = request.args.get('b', type=str)
-    b = "sum of " + val1 + " and " + val2 + " is: " + str(addition(val1, val2))
-    return b
-
-    
-
+    b = (addition(val1, val2))
+    d=jsonify({'description':'sum calculate',
+                'output':b})
+    return d
+@app.route('/prime')
+def checkprime():
+    num=request.args.get('a',type=str)
+ #   b="the " + num+ " is" + str(prime(num)) + " number."
+#    return b
+    b=prime(num)  
+    c=jsonify({'description':'prime calculation',
+               'output':b})
+    return c
 if __name__ == '__main__':
-   app.run()
+    app.run()
