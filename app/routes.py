2b5ac59c174e9d58f945de2e7034b0953f154257
jhilikkundu <jhilikkundu@localhost.localdomain>
2020-04-26 17:56:34 +0530

    output organized

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,jsonify
 from basic_func.prime import prime
 from basic_func.func1 import addition
+from basic_func.utility.arrange_op import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,17 +20,16 @@ def sumofnumbers():
     val1 = request.args.get('a',type = str)
     val2 = request.args.get('b', type=str)
     b = (addition(val1, val2))
-    d=jsonify({'description':'sum calculate',
-                'output':b})
+    d=json("result of sum",b)
     return d
 @app.route('/prime')
 def checkprime():
     num=request.args.get('a',type=str)
  #   b="the " + num+ " is" + str(prime(num)) + " number."
 #    return b
-    b=prime(num)  
-    c=jsonify({'description':'prime calculation',
-               'output':b})
+    b=(prime(num))  
+    c=json('prime calculation',b)
     return c
+
 if __name__ == '__main__':
     app.run()
