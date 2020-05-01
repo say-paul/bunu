f1486c1b5de37c0a356fdb999f389584c5a84ffc
jhilikkundu <jhilikkundu@localhost.localdomain>
2020-05-01 12:23:18 +0530

    recipe rough

--- app/Recipe/recipe_api.py
+++ app/Recipe/recipe_api.py
@@ -1,13 +1,35 @@
 import requests
 import json
-def find_recipe(arr):
+def find_recipe(a):
     urls="http://www.recipepuppy.com/api/"
-    params={'i':arr}
+    a=list(a.split(','))
+    params={'i':a}
     r=requests.get(url=urls,params=params)
-    return r.status_code
+    # return r.status_code
     data=json.loads(r.text)
-    return data["title"]
+   # print(data["results"])
+    #return len(array)
+    #ingredient=list()
+    #return len(data["results"])
 
-arr='onion,garlic'
+    count=0
+    extra=[]
+    for i in range(len(data["results"])):
+        ingre=list(data["results"][i]["ingredients"].split(','))
+        for j in range(len(a)):
+            if a[j] in ingre:
+                count+=1
+        extra.append(len(ingre)-count)
+        count=0 
+    print(extra)
+    m=min(extra)
+    final=[]
+    for i in range(len(extra)):
+        if m==extra[i]:
+            final.append({'title':data["results"][i]['title'],'link':data["results"][i]["href"]})
+    return final
+    
 
-print(find_recipe(arr))
+a ='garlic,chicken'
+
+print(find_recipe(a))
