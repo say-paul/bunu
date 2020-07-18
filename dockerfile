6ea6b2fbc992966d44a0c45241c1cd006de2a1b9
saypaul <saypaul@redhat.com>
2020-07-18 18:29:39 +0530

    added changes

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.8
 # RUN git config --global http.sslVerify false
 # RUN git clone -b flask_prime  https://github.com/say-paul/bunu.git testing
-COPY $PWD/testing/app /app
+COPY ./testing/app /app
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 app="docker.test"
 git clone -b flask_prime  https://github.com/say-paul/bunu.git testing
-docker build -t ${app} .
+docker build -t ${app} . -f dockerfile
 docker run -d -p 80:80 \
     --name=${app} ${app}
     # -v $PWD/app:/app ${app}
